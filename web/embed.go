86dad81b09f6432005d5893239cd4bfbd0d71cea
shus <153152554+yzlu0917@users.noreply.github.com>
2026-04-02 09:28:38 +0800

    fix: avoid web embed build failures (#391)

--- web/embed.go
+++ web/embed.go
@@ -5,6 +5,7 @@ package web
 import (
 	"embed"
 	"io/fs"
+	"log/slog"
 
 	"github.com/chenhg5/cc-connect/core"
 )
@@ -15,7 +16,12 @@ var distFS embed.FS
 func init() {
 	sub, err := fs.Sub(distFS, "dist")
 	if err != nil {
-		panic("web: embed dist: " + err.Error())
+		slog.Warn("web: embedded dist directory unavailable; web admin disabled", "error", err)
+		return
+	}
+	if _, err := fs.Stat(sub, "index.html"); err != nil {
+		slog.Warn("web: embedded dist assets missing index.html; web admin disabled", "error", err)
+		return
 	}
 	core.RegisterWebAssets(sub)
 }
